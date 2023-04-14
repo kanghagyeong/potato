e8b3454f45fa9a0790c6a4fa2f7a8b009147fbe8
kanghagyeong <130727384+kanghagyeong@users.noreply.github.com>
2023-04-14 13:19:34 +0900

    Add files via upload

---
+++ face-emotion-recognition-master/script.js
@@ -0,0 +1,60 @@
+const video = document.getElementById('video');
+
+// 얼굴 표정 관련 모델 데이터 로드
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+]).then(startVideo);
+
+// 모델 데이터 로드 완료 시 유저에게 비디오 권한 받아옴
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    (stream) => {
+      video.srcObject = stream;
+      sendMsgToChild('permission');
+    },
+    (err) => console.error(err)
+  );
+}
+
+// 실시간으로 웹캠 인식 후 표정 데이터 받아옴
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    // faceapi.draw.drawDetections(canvas, resizedDetections);
+    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    sendMsgToChild(resizedDetections);
+  }, 100);
+});
+
+// 게임 iframe창으로 메시지 전달
+function sendMsgToChild(msg) {
+  document.querySelector('iframe').contentWindow.postMessage(msg, '*');
+}
+
+// 게임 iframe창으로 부터 메시지 수신
+function receiveMsgFromChild(e) {
+  if (e.data.stage > 0) {
+    document.querySelector('img').src = `./image/${e.data.stage}.png`;
+    document.getElementById('video').style.display = 'block';
+    document.getElementById('imgContainer').style.display = 'flex';
+  } else {
+    document.getElementById('video').style.display = 'none';
+    document.getElementById('imgContainer').style.display = 'none';
+  }
+}
+
+window.addEventListener('message', receiveMsgFromChild);
